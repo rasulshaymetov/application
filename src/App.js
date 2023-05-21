7bdaec4f4e896791bff0009e481379ea6c76066c
Rasul <jolyskytner@gmail.com>
2023-05-21 21:15:37 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles/index.scss";
+import "./styles/variables.scss";
+import Main from "./components/pages/Main";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
