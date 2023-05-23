f5bcab425c24847d60c9d17d2e3d449abe7ce9be
Rasul <jolyskytner@gmail.com>
2023-05-23 15:10:20 +0500

    commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Post = (props) => {
   return (
-        <div className="main__post mb-50 p-25 border-blue">
+        <div className="main__post mb-50 p-25 border-blue white">
             <div className="post__content">
-                <strong>{props.number}.{props.post.title}</strong>
+                <div className='flex'><strong>{props.number}</strong><p>. {props.post.title}</p></div>
                 <div>{props.post.body}</div>
             </div>
             <div className="post_btn">
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,45 +1,60 @@
-import React, { useState } from 'react'
-import Postlist from '../Postlist'
-import MyInput from '../UI/input/MyInput'
-import MyButton from '../UI/button/MyButton'
+import React, { useState, useEffect } from "react";
+import Postlist from "../Postlist";
+import MyInput from "../UI/input/MyInput";
+import MyButton from "../UI/button/MyButton";
 
 const Main = () => {
-  const [posts, setPosts] = useState([])
-  const [post, setPost] = useState({title: '', body: ''})
-
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({ title: "", body: "" });
+  const [isDisabled, setIsDisabled] = useState(false);
   const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id:Date.now()}])
-    setPost({title: '', body: ''})
-    console.log("posts:", posts)
-  }
+    e.preventDefault();
+    setPosts([...posts, { ...post, id: Date.now() }]);
+    setPost({ title: "", body: "" });
+  };
+  useEffect(() => {
+    if (!post.title) {
+      console.log("Поле пустое");
+      setIsDisabled(true);
+    } else if(post.title) {
+      console.log("Поле не пустое");
+      setIsDisabled(false);
+    }
+  }, [post]);
+
   return (
     <div>
+<button className="spec-btn" onClick={() => console.log(posts)}>Вывести в консоль информацию о всех постах</button>
       <form>
-     <div className="flex-center column m-auto center">
-     <MyInput
-        value={post.title}
-        onChange={(e) => setPost({...post, title: e.target.value})}
-        type='text'
-        name='title'
-        placeholder='Название поста'
-        />
-         <MyInput
-        value={post.body}
-        onChange={(e) => setPost({...post, body: e.target.value})}
-        type='text'
-        name='body'
-        placeholder='Описание поста'
-        />
-        
-        <MyButton onClick={addNewPost} type='sumbit'>Опубликовать пост</MyButton>
-     </div>
+        <div className="flex-center column m-auto center">
+          <MyInput
+            value={post.title}
+            onChange={(e) => setPost({ ...post, title: e.target.value })}
+            type="text"
+            name="title"
+            placeholder="Название поста"
+          />
+          <MyInput
+            value={post.body}
+            onChange={(e) => setPost({ ...post, body: e.target.value })}
+            type="text"
+            name="body"
+            placeholder="Описание поста"
+          />
+          <MyButton
+            disabled={isDisabled ? true : null}
+            onClick={addNewPost}
+            type="sumbit"
+          >
+            Опубликовать пост
+          </MyButton>
+        </div>
       </form>
       <div className="main__posts flex-center">
-      <Postlist posts={posts} title={'Посты'}/>
+        <Postlist posts={posts} title={"Посты"} />
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Main
+export default Main;
